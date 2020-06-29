4c6b97783ae95bfa7cb3a5731869fc6d7346c59a
Kevin Karsopawiro <kevin@ubuntu.fritz.box>
2020-06-29 22:50:03 +0000

    Updates

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,17 @@
 const path = require('path')
 const downloadTorrent = require('../server/jobs/download-torrent')
+const { downloadFile } = require('../server/helpers/download-helper')
 
-const p = downloadTorrent(path.join(__dirname, 'test.torrent'), path.join(__dirname, 'test-result'))
-p.on('start', () => console.log('download started'))
-p.on('progress', (torrent) => console.log('progress'))
-p.on('done', () => console.log('download finished'))
+const url = process.argv[2]
+
+const run = async () => {
+  const p = (url)
+    ? downloadTorrent(url, path.join(__dirname, 'test-result'))
+    : downloadTorrent(path.join(__dirname, 'test.torrent'), path.join(__dirname, 'test-result'))
+  p.on('start', () => console.log('download started'))
+  p.on('progress', (torrent) => console.log('progress'))
+  p.on('done', () => console.log('download finished'))
+  p.on('error', (err) => console.log(err))
+}
+
+run()
